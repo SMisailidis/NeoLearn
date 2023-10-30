489ff9886eb3e55e742e5b0ade27b28c03a82231
PaulosMpou <paulosmpou02@gmail.com>
2023-10-30 02:25:38 +0200

    Added Log In Page - Finished Log In UI

---
+++ partials/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../Front-End/css/header.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+    <header class="container-fluid  header">
+
+        <div class="col-12 col-lg-6 col-xl-4 logoContainer">
+            <img src="../images/logo.png" alt="">
+            <span>NEOLEARN</span>
+            <span>'Fueling Curiosity, Igniting Success' </span>
+        </div>
+    </header>
+    <div class="subheader container-fluid"></div>
+
+
+
+</body>
+
+</html>
