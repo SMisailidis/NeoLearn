17262a42f4b399495bf7946c85862a0eedbc4897
PaulosMpou <paulosmpou02@gmail.com>
2023-11-04 19:00:40 +0200

    fixed Navigation Behaviour on resize

--- assets/js/navbarToggle.js
+++ assets/js/navbarToggle.js
@@ -3,31 +3,54 @@ $(document).ready(function () {
   let menuButton = $(".menuBtn");
   let menuState = false;
   let menuIcon = $("#menuIcon");
+  let portfolioContainer = $('.mainScreenPortfolioContainer');
+  let portfolio = $('.contentPortfolioContainer');
   function openMenu() {
     menuState = true;
-    navigationBar.addClass("navActive activateWidth");
+    navigationBar.css("flex-basis", "60%");
+    portfolioContainer.addClass('openMenuContainerResponse');
+    portfolio.addClass('openMenuPortfolioResponse');
     menuIcon.removeClass("fa-bars").addClass("fa-x");
   }
+
   function closeMenu() {
     menuState = false;
-    navigationBar.removeClass("navActive activateWidth");
+    navigationBar.css("flex-basis", "0%");
+    removeTransitionClasses(500);
     menuIcon.removeClass("fa-x").addClass("fa-bars");
   }
+
+  function removeTransitionClasses(delay) {
+    setTimeout(() => {
+      portfolioContainer.removeClass('openMenuContainerResponse');
+      portfolio.removeClass('openMenuPortfolioResponse');
+    }, delay);
+  }
+
   menuButton.on("click", function () {
-    if (navigationBar.css("flex-basis") === "0%") {
-      navigationBar.css("flex-basis", "25%");
-    } else {
-      navigationBar.css("flex-basis", "0%");
-    }
-    if (navigationBar.css("flex-basis") === "0%") {
+    if (!menuState)
       openMenu();
+    else
+      closeMenu();
+  });
+
+  $(window).on("resize", function () {
+    if (window.innerWidth > 768) {
+      if (menuState) {
+        openMenu();
+      } else {
+        navigationBar.css("flex-basis", "20%");
+      }
     } else {
       closeMenu();
     }
   });
-  $(window).on("resize", function () {
-    if (this.innerWidth > 768) {
-      closeMenu();
+
+  window.onresize = function () {
+    if (window.innerWidth > 768) {
+      removeTransitionClasses(250);
+      navigationBar.css("flex-basis", "20%");
+
     }
-  });
+  };
 });
