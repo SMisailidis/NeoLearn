4389eab49f0db26a04c60e889e5f95fc7b475163
KeyboardSmasher123 <105115540+KeyboardSmasher123@users.noreply.github.com>
2023-11-24 18:55:08 +0200

    Design Changes

--- MyCourses.php
+++ MyCourses.php
@@ -2,22 +2,24 @@
 <main>
     <h1>My Courses</h1>
     <div class="container">
-        <table>
-            <tr class="TRow">
-                <td class="CourseCol">COURSE-1</td>
-                <td class="IconCol"><a id="editButton" class="ENbuttons"><img class="ENicons" src="assets/images/edit.png" width="30" height="30"></a></td>
-                <td class="IconCol"><a id="notesButton" class="ENbuttons"><img class="ENicons" src="assets/images/notes.png" width="30" height="30"></a></td></td>
-            </tr>
-            <tr class="TRow">
-                <td class="CourseCol">COURSE-1</td>
-                <td class="IconCol"><a id="editButton" class="ENbuttons"><img class="ENicons" src="assets/images/edit.png" width="30" height="30"></a></td>
-                <td class="IconCol"><a id="notesButton" class="ENbuttons"><img class="ENicons" src="assets/images/notes.png" width="30" height="30"></a></td></td>
-            </tr>
-        </table>
+        <div class="CoursesList">
+            <div class="CourseLine">
+                <a class="CourseName">COURSE-1</a>
+                <a id="editButton" class="ENbuttons"><img class="ENicons" src="assets/images/edit.png" width="30" height="30"></a>
+                <a id="notesButton" class="ENbuttons"><img class="ENicons" src="assets/images/notes.png" width="30" height="30"></a>
+            </div>
+            <div class="CourseLine">
+                <a class="CourseName">COURSE-2</a>
+                <a id="editButton" class="ENbuttons"><img class="ENicons" src="assets/images/edit.png" width="30" height="30"></a>
+                <a id="notesButton" class="ENbuttons"><img class="ENicons" src="assets/images/notes.png" width="30" height="30"></a>
+            </div>
+        </div>  
         <div class="button-container">
-            <div class="Buttons">Add a Course</div> 
-            <div class="Buttons">Remove a Course</div>
-        </div>     
+            <div id="AddB" class="Buttons">Add a Course</div> 
+            <div id="RemoveB" class="Buttons">Remove a Course</div>
+        </div> 
     </div>
 </main>
+
+
 
