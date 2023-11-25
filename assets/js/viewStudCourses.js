b3796a76a82ad5ce95dfd5eeef546b839a13f2da
Azemis Ioannis <ics21175@uom.edu.gr>
2023-11-25 14:36:55 +0200

    fixes on viewStudCourses.js and php

--- assets/js/viewStudCourses.js
+++ assets/js/viewStudCourses.js
@@ -1,34 +1,34 @@
 import { fetchData } from "./eventHandler.js";
 
 $(document).ready(function(){
-    function updateCourseList(studID){
+    
+    function updateCourses(studentID){
 
 
-
-        fetchData($, "assets/viewStudCourses.php", 'GET', {studentID: studID})
+        fetchData($, "assets/Back-End/getEnrolledStudents.php", 'GET', {studentID: studID})
     .then(response => {
-      const coursesContainer = $("#coursesContainer");
+
+        const leftChildDiv = $(".left-child");
 
       $.each(response, function (index, course) {
         const courseDiv = $("<div>").addClass("rectangle");
-
         const courseTitle = $("<a>").addClass("courseTitle").attr("href", course.ID).text(course.Title);
-
         const viewCourse = $("<a>").addClass("viewCourse").attr("href", "#");
-
         const arrowImage = $("<img>").attr("src", "./assets/images/whiteRightarrow.png").attr("alt", "arrow");
 
         viewCourse.append(arrowImage);
-
         courseDiv.append(courseTitle).append(viewCourse);
+        leftChildDiv.append(courseDiv);
 
-        coursesContainer.append(courseDiv);
       });
     })
     .catch(error => {
       console.error('Error fetching course data:', error);
     });
-}
-const studID = JSON.parse(sessionStorage.getItem("userData"))
+
+    }
+
+    const studentID = JSON.parse(sessionStorage.getItem("userData"))[0].ID
+    updateCourses(studentID)
 
 });
--- viewStudCourses.php
+++ viewStudCourses.php
@@ -1,31 +1,15 @@
 <?php require 'layout.php' ?>
 <main class="mainScreenViewStudCoursesContainer">
   <div class="central-div">
-      <div class="left-child">
-        <div class="rectangle">
-          <a class="courseTitle" href="./viewCourseNotes.php">COURSE 1 - (CS409)</a>
-          
-          <a class="viewCourse" href="./viewCourseNotes.php"><img src="./assets/images/whiteRightarrow.png" alt="arrow"></a>
-        </div>
-        <div class="rectangle">
-          <a class="courseTitle" href="">COURSE 1 - (CS409)</a>
-          <a class="viewCourse" href=""><img src="./assets/images/whiteRightarrow.png" alt="arrow"></a>
-        </div>
-        <div class="rectangle">
-          <a class="courseTitle" href="">COURSE 1 - (CS409)</a>
-          <a class="viewCourse" href=""><img src="./assets/images/whiteRightarrow.png" alt="arrow"></a>
-        </div>
-        <div class="rectangle">
-          <a class="courseTitle"  href="">COURSE 1 - (CS409)</a>
-          <a class="viewCourse" href=""><img src="./assets/images/whiteRightarrow.png" alt="arrow"></a>
-        </div>
-      </div>
-      <div class="right-child">
-        <button>Add Course</button>
-        <button>Remove Course</button>
-      </div>
+  <div class="left-child" id="leftChildContainer">
     </div>
-          </div>
+
+    <div class="right-child">
+      <button>Add Course</button>
+      <button>Remove Course</button>
+    </div>
+  </div>
+</div>
 </main>
     </body>
 </html>
