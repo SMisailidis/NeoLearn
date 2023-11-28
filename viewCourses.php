acdb848943d21d854431ec6672d4edba495db0ac
KeyboardSmasher123 <105115540+KeyboardSmasher123@users.noreply.github.com>
2023-11-28 21:22:57 +0200

    some css changes

--- viewCourses.php
+++ viewCourses.php
@@ -3,6 +3,7 @@
     <h1 class="header-text">My Courses</h1>
     <div class="container">
         <div class="CoursesList">
+            
         </div>  
         <div class="button-container">
             <a href="addCourse.php"><button id="AddB" class="Buttons">Add a Course</button></a> 
