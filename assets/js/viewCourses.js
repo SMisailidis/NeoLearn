b282b8802c92bfb334c5e50a00deb1c9222ac3fe
KeyboardSmasher123 <105115540+KeyboardSmasher123@users.noreply.github.com>
2023-12-02 19:45:08 +0200

    JS and UI changes
    The confirm button is disabled until a course is selected

--- assets/js/viewCourses.js
+++ assets/js/viewCourses.js
@@ -9,17 +9,22 @@ $(document).ready(function () {
             .then((response) => {
 
                 $.each(response, function (index, course) {
-                    const CourseLine = $("<div>").addClass("CourseLine")
-                    const name = $("<a>").addClass("CourseName").attr("href", "").append(course.Title)
-                    const editAnchor = $("<a>").addClass("ENbuttons").attr("id", "editButton").attr("href", "")
-                    const editImage = $("<img>").addClass("ENicons").attr("src", "assets/images/edit.png").attr("width", "30").attr("height", "30")
+                    const CourseLine = $("<div>").addClass("CourseLine");
+                    const name = $("<a>").addClass("CourseName").attr("href", "").append(course.Title);
+                    const editAnchor = $("<a>").addClass("ENbuttons").attr("id", "editButton").attr("href", "");
+                    const editImage = $("<img>").addClass("ENicons").attr("src", "assets/images/edit.png").attr("width", "30").attr("height", "30");
 
-                    const editButton = editAnchor.append(editImage)
+                    const editButton = editAnchor.append(editImage);
 
                     const checkbox = $("<input>").attr("type", "checkbox").val(course.ID).addClass("courseCheckbox");
                 
                     CourseLine.append(name).append(editButton).append(checkbox);
-                    coursesList.append(CourseLine)
+                    coursesList.append(CourseLine);
+                });
+
+                $(".courseCheckbox").on("change", function () {
+                    const atLeastOneCheckboxChecked = $(".courseCheckbox:checked").length > 0;
+                    $("#confirmB").prop("disabled", !atLeastOneCheckboxChecked);
                 });
             })
             .catch((error) => {
@@ -27,46 +32,30 @@ $(document).ready(function () {
             });
     }
     const teacherID = JSON.parse(sessionStorage.getItem("userData"))[0].ID
-    updateCourseList(teacherID)
+    updateCourseList(teacherID);
+
+    function updateConfirmButtonState() {
+        const atLeastOneCheckboxChecked = $(".courseCheckbox:checked").length > 0;
+        $("#confirmB").prop("disabled", !atLeastOneCheckboxChecked);
+    }
 
     $("#RemoveB, #cancelB").click(function () {
         $(".Buttons, .RemoveButtons, .ENbuttons, .courseCheckbox").toggle();
+
+        if ($(this).attr("id") === "cancelB") {
+            $(".courseCheckbox").prop("checked", false);
+        }
+
+        updateConfirmButtonState();
     });
 
     $("#confirmB").click(function () {
-        const selectedCourses = $(".courseCheckbox:checked").map(function () {
+        const checkedCourseIDs = $(".courseCheckbox:checked").map(function () {
             return $(this).val();
         }).get();
-        
-        const ModalB = $("#confirmB");
 
-        if (selectedCourses.length === 0) {
-            ModalB.attr("data-target", "#noCourseSelectedModal");
-            $("#noCourseSelectedModal").modal('show');
-        } else {
-            ModalB.attr("data-target", "#confirmationModal");
-            $("#confirmationModal").modal('show');
-        }
+        console.log("Selected Course IDs:", checkedCourseIDs);
     });
-
-    $("#OKbutton").click(function () {
-        $("#noCourseSelectedModal").modal('hide');
-    });
-
-    $("#confirmDeleteBtn").click(function () {
-        const selectedCourses = $(".courseCheckbox:checked").map(function () {
-            return $(this).val();
-        }).get();
-        console.log(selectedCourses);
-
-        if (selectedCourses.length === 0) {
-            console.log("No courses selected for deletion");
-            return;
-        }
-    });
-
-    
-
 });
 
 
