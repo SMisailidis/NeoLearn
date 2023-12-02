efbb6daa4f4c5e36e48ca55f92d2cabbc3ee2625
Azemis Ioannis <ics21175@uom.edu.gr>
2023-12-02 17:55:26 +0200

    fixes in viewCoursesNotes.css, viewNotesDetails.css

--- viewCourseNotes.php
+++ viewCourseNotes.php
@@ -7,19 +7,15 @@
     <div class="allRectangles">
         <div class="Notesrectangle">
             <a class="chapterNotesTitle" href="./viewNotesDetails.php">Chapter 1 Notes</a>
-            <a class="deleteNotes" href=""><img src="./assets/images/bin.png" alt=""></a>
         </div>
         <div class="Notesrectangle">
             <a class="chapterNotesTitle" href="">Chapter 2 Notes</a>
-            <a class="deleteNotes" href=""><img src="./assets/images/bin.png" alt=""></a>
         </div>
         <div class="Notesrectangle">
             <a class="chapterNotesTitle" href="">Chapter 3 Notes</a>
-            <a class="deleteNotes" href=""><img src="./assets/images/bin.png" alt=""></a>
         </div>
         <div class="Notesrectangle">
             <a class="chapterNotesTitle" href="">Chapter 4 Notes</a>
-            <a class="deleteNotes" href=""><img src="./assets/images/bin.png" alt=""></a>
         </div>
     </div>
   </div>
