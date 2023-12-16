4c18ea4189791a734a0bb3722a3cb2e6409e9d67
Azemis Ioannis <ics21175@uom.edu.gr>
2023-12-16 18:25:17 +0200

    video Display added css and html(.php)

---
+++ videoDisplay.php
@@ -0,0 +1,7 @@
+<?php require 'layout.php' ?>
+        <iframe class="videoClass" width="928" height="522" 
+        src="https://www.youtube.com/embed/_t95_6DFN4M" 
+        title="Poutana (Remastered)"
+         frameborder="0" 
+         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen></iframe>
