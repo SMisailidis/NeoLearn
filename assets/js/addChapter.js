f45ad8c984d145d7f5c4853785169378f25e6242
KeyboardSmasher123 <105115540+KeyboardSmasher123@users.noreply.github.com>
2023-12-16 17:57:28 +0200

    Update addChapter.js

--- assets/js/addChapter.js
+++ assets/js/addChapter.js
@@ -1,23 +1,61 @@
-import { fetchData } from "./EventHandler";
-import { uploadPDF } from "./uploadPDF";
-import toast from "./toast";
-import modal from "./modal";
+import { fetchData } from "./eventHandler.js";
+import modal from "./modal.js";
+import toast from "./toast.js";
 
-$(document).ready(function() {
+$(document).ready(function () {
+  let chapterData = {};
+
+  const onSubmitHandler = (e) => {
+    const inputs = $("form.addDetails :input");
+    e.preventDefault();
+
     let urlParams = new URLSearchParams(window.location.search);
     let courseId = urlParams.get('courseId');
+    chapterData = {
+      Course_ID: courseId,
+      ID: inputs[1].value,
+      Title: inputs[0].value,
+      Description: inputs[2].value,
+      Video_Link: inputs[3].value,
+    };
 
-    let cur_id = $("chapterID").value;
-    let cur_title = $("chapterTitle").value;
-    let cur_desc = $("chapterDesc").value;
+    modal.openModal();
+  };
 
-    function addNewChapter(){
-        fetchData($, "assets/Back-End/addNewChapter.php", "POST", {Course_ID : courseId, ID : cur_id, Title : cur_title, Description : cur_desc})
-        .then((response) =>{
-            
-        })
-        .catch((error)=>{
-            console.log("Error creating chapter: " + error)
-        });
-    }
-})
+  const resetInputs = () => {
+    const inputs = $("form.addDetails :input");
+
+    $.each(inputs, function (index, row) {
+      row.value = "";
+    });
+  };
+
+  modal.onClickCloseHandler(() => {
+    resetInputs();
+  });
+
+  modal.onClickSaveHandler(() => {
+    chapterData.Pdf_Link = "example.pdf";
+
+    fetchData(jQuery, "assets/Back-End/addChapter.php", "POST", chapterData)
+      .then((success) => {
+        if (success) {
+          toast.showToast();
+          modal.openModal();
+          modal.closeModal();
+          resetInputs();
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+
+  $("form.addDetails").on("submit", onSubmitHandler);
+
+  modal.setTitle("Add Chapter");
+  modal.setContent("Do you want to publish the Chapter?");
+  modal.setButtonsText("No", "Yes");
+
+  toast.setContent("Published successfully!");
+});
