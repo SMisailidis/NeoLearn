fcba4e8cea7d5ed971dc5af7271783aab18ab2dc
PaulosMpou <128242056+PaulosMpou@users.noreply.github.com>
2023-12-26 18:51:53 +0200

    a

--- layout.php
+++ layout.php
@@ -65,6 +65,7 @@
         <link rel="stylesheet" href="assets/css/quiz.css">
         <link rel="stylesheet" href="assets/css/changePassword.css">
         <link rel="stylesheet" href="assets/css/videoDisplay.css">
+        <link rel="stylesheet" href="assets/css/quizLandingPage.css">
         <!---------------->
 
         <!-- JS assets -->
