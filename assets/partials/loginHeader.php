da6d308ffff899f00a2a7bd275689334970bf8ba
PaulosMpou <128242056+PaulosMpou@users.noreply.github.com>
2023-12-29 00:00:18 +0200

    loginPage animation

--- assets/partials/loginHeader.php
+++ assets/partials/loginHeader.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="assets/css/loginHeader.css">
     <link rel="stylesheet" href="assets/css/globals.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script
@@ -18,8 +19,8 @@
 </head>
 
 <body>
-    <header class="container-fluid  header">
-        <div class="col-12 col-lg-6 col-xl-4 logoContainer">
+    <header class="container-fluid header">
+        <div class="col-12 col-lg-6 col-xl-4 logoContainer animate__animated animate__fadeInDown">
             <img src="assets/images/logo.png" alt="">
             <span>NEOLEARN</span>
             <span>'Fueling Curiosity, Igniting Success'</span>
